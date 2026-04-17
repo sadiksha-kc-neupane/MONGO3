62f23d8c7403b7aac784a832dbec3966425c9407
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-04-17 18:57:53 +0545

    styling added and linking fixed

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Chat = require("./models/chat");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
   .then(() => `Connected to MongoDB`)
