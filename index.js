eff7d57409193b13506e7d97c626eb19461b662c
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-04-17 20:58:06 +0545

    from added to add new chat using it in database

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const Chat = require("./models/chat");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 main()
   .then(() => `Connected to MongoDB`)
@@ -41,6 +42,27 @@ app.get("/chats", async (req, res) => {
 
 app.get("/chats/new", (req, res) => {
   res.render("new");
+});
+
+app.post("/chats", (req, res) => {
+  let { from, to, message } = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    message: message,
+    createdAt: new Date(),
+  });
+
+  newChat
+    .save()
+    .then(() => {
+      console.log("Chat saved to database");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  res.redirect("/chats");
 });
 
 app.listen(port, () => {
