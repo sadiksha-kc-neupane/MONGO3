cdbf00d06bfaaeeebb75c1231f1a5716fab77ed1
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-04-19 16:22:20 +0545

    new route added for updating message

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
-
 const path = require("path");
 const Chat = require("./models/chat");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
   .then(() => `Connected to MongoDB`)
@@ -36,7 +37,7 @@ app.get("/", (req, res) => {
 
 app.get("/chats", async (req, res) => {
   let chats = await Chat.find();
-  console.log(chats);
+  //console.log(chats);
   res.render("index", { chats });
 });
 
@@ -71,6 +72,20 @@ app.get("/chats/:id/edit", async (req, res) => {
   let { id } = req.params;
   let chat = await Chat.findById(id);
   res.render("edit", { chat });
+});
+
+//Update route
+app.put("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let { message: newMessage } = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    { message: newMessage },
+    { runValidators: true }, //to run the validators defined in the schema
+    { new: true }, //to return the updated document instead of the old one
+  );
+  res.redirect("/chats");
+  console.log(updatedChat);
 });
 
 app.listen(port, () => {
